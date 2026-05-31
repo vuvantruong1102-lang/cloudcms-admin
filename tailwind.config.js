023d9f1b9de97c9daa49a190411826b035c9975d
vuvantruong1102-lang <vuvantruong.1102@gmail.com>
2026-05-31 10:51:47 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {
+      screens: {
+        xs: '420px',
+      },
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
       },
